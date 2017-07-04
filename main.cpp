2541fe434d2a5c863edbf5e831a9221e096cfbf7
Shengwen Cheng <shengwen1997.tw@gmail.com>
2017-07-04 21:09:39 +0800

    Do tag position visualization

--- main.cpp
+++ main.cpp
@@ -28,6 +28,9 @@ int main()
 	float start, end;
 	float duration, fps;
 
+	CvPoint p1, p2, p3, p4;
+	CvPoint tag_center;
+
 	while(video_capture.read(raw_image)) {
 		//start timer
 		start = getTickCount();
@@ -38,9 +41,6 @@ int main()
 		//detect Apriltags
 		vector<AprilTags::TagDetection> detections = tag_detector.extractTags(grey_image);		
 
-		cv::imshow("window", raw_image);
-		cv::waitKey(30);
-
 		//stop timer
 		end = getTickCount();
 
@@ -49,10 +49,35 @@ int main()
 		fps = 1.0f / duration;
 
 		if(detections.size() > 0) {
+			p1.x = detections.at(0).p[0].first;
+			p1.y = detections.at(0).p[0].second;
+
+			p2.x = detections.at(0).p[1].first;
+			p2.y = detections.at(0).p[1].second;
+
+			p3.x = detections.at(0).p[2].first;
+			p3.y = detections.at(0).p[2].second;
+
+			p4.x = detections.at(0).p[3].first;
+			p4.y = detections.at(0).p[3].second;
+
+			tag_center.x = detections.at(0).cxy.first;
+			tag_center.y = detections.at(0).cxy.second;
+
+			cv::line(raw_image, p1, p2, Scalar(0, 255, 0), 2, CV_AA);
+			cv::line(raw_image, p2, p3, Scalar(0, 0, 255), 2, CV_AA);
+			cv::line(raw_image, p3, p4, Scalar(255, 0, 0), 2, CV_AA);
+			cv::line(raw_image, p4, p1, Scalar(255, 0, 0), 2, CV_AA);
+
+			cv::circle(raw_image, tag_center, 2, Scalar(0, 0, 255), 2, CV_AA, 0);
+
 			printf("[fps: %.2f] tag detected, id = %d.\n", fps, detections.at(0).id);
 		} else {
 			printf("[fps: %.2f] no tag was found.\n", fps);	
 		}
+
+		cv::imshow("window", raw_image);
+		cv::waitKey(30);
 	}
 
 	return 0;
